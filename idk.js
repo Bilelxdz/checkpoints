52593efa7875a17a06712fecde29cee1f384a2a0
Bilelxdz <35871833+Bilelxdz@users.noreply.github.com>
2020-11-05 15:14:31 +0100

    Add files via upload

---
+++ idk.js
@@ -0,0 +1,18 @@
+arr = [1,5,3,9,0,10]
+let l;
+for (let i = 0; i < arr.length; i++) {
+    l = i
+    for (let j = i-1; j >=0; j--) {
+        if(arr[l]< arr[j]){
+           [ arr[j] , arr[l] ]= [arr[l] , arr[j]]
+           l--
+        }/*else{
+            [arr[i] , arr[j]] = [arr[j] , arr[i]]
+       }*/
+    }
+    
+    
+}
+// [a,b] = [b,a]
+
+// const [a,b] 
